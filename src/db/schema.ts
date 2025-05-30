f58c23a77baa427cf8df7f03db1db9ba84166c3d
Yukiharu YABUKI <yabuki@netfort.gr.jp>
2025-05-31 06:13:40 +0900

    Database Schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,38 +1,45 @@
-import { pgTable, integer, char, timestamp, smallint, varchar, pgSequence, serial, text, boolean, foreignKey } from "drizzle-orm/pg-core"
+import { pgTable, unique, serial, text, integer, date, smallint, numeric, varchar } from "drizzle-orm/pg-core"
 import { sql } from "drizzle-orm"
 
 
-export const sampleIdSeq = pgSequence("sample_id_seq", {  startWith: "1", increment: "1", minValue: "1", maxValue: "9223372036854775807", cache: "1", cycle: false })
 
-export const sample = pgTable("sample", {
-	id: integer().primaryKey().notNull(),
-	name: char({ length: 100 }).notNull(),
-	createdDateTime: timestamp("created_date_time", { mode: 'string' }).default(sql`CURRENT_TIMESTAMP`).notNull(),
-});
+export const tableKind = pgTable("table_kind", {
+	id: serial().primaryKey().notNull(),
+	tableName: text("table_name").notNull(),
+}, (table) => [
+	unique("table_kind_table_name_key").on(table.tableName),
+]);
+
+export const youkou = pgTable("youkou", {
+	id: serial().primaryKey().notNull(),
+	shubetu: integer().notNull(),
+	begindate: date().notNull(),
+	enddate: date(),
+}, (table) => [
+	unique("youkou_shubetu_begindate_key").on(table.shubetu, table.begindate),
+]);
+
+export const kenpoMoney = pgTable("kenpo_money", {
+	tableVersion: smallint("table_version").notNull(),
+	grade: smallint().notNull(),
+	base: numeric({ precision: 14, scale:  2 }).notNull(),
+	lowervalue: numeric({ precision: 14, scale:  2 }),
+	highvalue: numeric({ precision: 14, scale:  2 }),
+}, (table) => [
+	unique("kenpo_money_table_version_grade_base_key").on(table.tableVersion, table.grade, table.base),
+]);
+
+export const kouseiMoney = pgTable("kousei_money", {
+	tableVersion: smallint("table_version").notNull(),
+	grade: smallint().notNull(),
+	base: numeric({ precision: 14, scale:  2 }).notNull(),
+	lowervalue: numeric({ precision: 14, scale:  2 }),
+	highvalue: numeric({ precision: 14, scale:  2 }),
+}, (table) => [
+	unique("kousei_money_table_version_grade_base_key").on(table.tableVersion, table.grade, table.base),
+]);
 
 export const jisX0401 = pgTable("jis_x0401", {
 	prefectureId: smallint("prefecture_id").primaryKey().notNull(),
 	name: varchar({ length: 10 }).notNull(),
 });
-
-// export const dinosaurs = pgTable("dinosaurs", {
-//   id: serial().primaryKey().notNull(),
-//   name: text(),
-//   description: text(),
-// });
-
-// export const tasks = pgTable("tasks", {
-//   id: serial().primaryKey().notNull(),
-//   dinosaurId: integer("dinosaur_id"),
-//   description: text(),
-//   dateCreated: timestamp("date_created", { mode: "string" }).defaultNow(),
-//   isComplete: boolean("is_complete"),
-// }, (table) => {
-//   return {
-//     tasksDinosaurIdFkey: foreignKey({
-//       columns: [table.dinosaurId],
-//       foreignColumns: [dinosaurs.id],
-//       name: "tasks_dinosaur_id_fkey",
-//     }),
-//   };
-// });
